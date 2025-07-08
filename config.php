a0f4a89593df6b310c69d682f2fdd65adbf6d430
AYESHA2-JOHRI <ayeshajohri28@gmail.com>
2025-07-08 12:28:23 +0500

    login register is ready with basic home page

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$databas = "myschooldb";
+
+$conn = new mysqli($host, $user, $password, $databas);
+
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+?>
---
+++ login_registration.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+require_once 'config.php';
+
+if(isset($_POST['register'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $mobileNumber = $_POST['mobileNumber'];
+
+    //echo 'form data=  name: '.$name.' email: '.$email. 'password: '.password. ' mobileNumber: '.mobileNumber;
+    $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email'");
+    //echo 'check email query result: '.$checkEmail;
+    if ($checkEmail->num_rows > 0) {
+        $_SESSION['register_error'] = 'Email is already registered!';
+        $_SESSION['active_form'] = 'register';
+    } else {
+        $conn->query("INSERT INTO users (name, email, password, mobileNumber) VALUES ('$name','$email','$password','$mobileNumber')");
+    }
+
+    header("Location: index.php");
+    exit();
+}
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+    if($result->num_rows > 0){
+        $user = $result->fetch_assoc();
+        if(password_verify($password, $user['password'])){
+            $_SESSION['name']= $user['name'];
+            $_SESSION['email']= $user['email'];
+            $_SESSION['mobileNumber']= $user['mobileNumber'];
+            header("Location: homePage.php");
+            exit();
+        }
+    }
+
+    $_SESSION['login_error']= 'Incorrect email or password';
+    $_SESSION['active_form']= 'login';
+    header("Location: index.php");
+    exit();
+
+}
+
+?>
